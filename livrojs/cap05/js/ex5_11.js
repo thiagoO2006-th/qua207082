91903950d3932b64b6ad006060e0c72e4fdba4af
DeathadderO11 <=>
2025-09-11 20:28:31 -0300

    Exercicios

---
+++ livrojs/cap05/js/ex5_11.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")()
+const produto = prompt("Produto: ")
+const num = Number(prompt("Nº de Etiquetas:"))
+const eLinha = Number(prompt("Nº de Etiquetas por linha: "))
+// cria um laço de repetição até num/2
+for(let i = 1; i < num/eLinha; i++){
+    let linha = ""
+    for(let j = 1; j <= eLinha; j++){
+        linha += `${produto.padEnd(30, '. ' ) } `   
+     }
+     console.log(linha)
+}
+let linha = ""
+for(let j = 1; j <= num%eLinha; j++){
+    linha += `${produto.padEnd(30, '. ' ) } `
+ }
+ console.log(linha)
