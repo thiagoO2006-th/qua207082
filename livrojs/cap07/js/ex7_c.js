3853ebdfa4ede0049fdf29cf2bd1e0ba31986218
thiagoO2006-th <thiagosz888888@gamil.com>
2025-09-30 20:02:07 -0300

    cap08 exemplo livro 8_1.html aula 30 09

--- livrojs/cap07/js/ex7_c.js
+++ livrojs/cap07/js/ex7_c.js
@@ -1,19 +1,18 @@
 const frm = document.querySelector("form")
-const resp1 = document.querySelector("outResp1")
-const resp2 = document.querySelector("outResp2")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
-    const dataInfracao = frm.inValorMulta.value.split("-")
+    const dataInfracao = frm.inDataInfracao.value.split("-")
     const valorMulta = Number(frm.inValorMulta.value)
     const dataDesconto = new Date()
     dataDesconto.setFullYear(Number(dataInfracao[0]))
-    dataDesconto.setMonth(dataInfracao[1] + 1)
-    dataDesconto.setDate(dataInfracao[2])
-    // dataDesconto = dataDesconto + (1000 * 60 * 60 * 24 * 90)
-    resp1.innerText = " Data Limite para pagamento com desconto: "
+    dataDesconto.setMonth(Number(dataInfracao[1]) - 1)
+    dataDesconto.setDate(Number(dataInfracao[2])+90)
+    resp1.innerText = "Data Limite para pagamento com desconto: "
     resp1.innerText += dataDesconto.getDate().toString().padStart(2, "0") + "/"
     resp1.innerText += (dataDesconto.getMonth() + 1).toString().padStart(2, "0") + "/"
     resp1.innerText += dataDesconto.getFullYear()
-    resp2.innerText = " Valor com descomto R$: " + (valorMulta * 0.8).toFixed(2)
+    resp2.innerText = " Valor com descomto R$: "+ (valorMulta * 0.8).toFixed(2)
 })
