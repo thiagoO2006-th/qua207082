213c4a646dbfd9495635c47c26d096410ff6dd87
thiagoO2006-th <thiagosz888888@gamil.com>
2025-10-02 21:40:27 -0300

    começo exercicio 8_A.html aula 02 10

--- exemplos/calculadora/js/script.js
+++ exemplos/calculadora/js/script.js
@@ -21,3 +21,32 @@ function calcular() {
         visor.value = "Erro"
     }
 }
+
+function porcentagem () {
+    calcular()
+    const visor = document.querySelector("#visor")
+    visor.value = Number(visor.value)/100
+}
+
+const botoes = document.querySelectorAll("button")
+for (const botao of botoes) {
+    botao.addEventListener("click", () =>{
+        switch (botao.innerText) {
+            case "C":
+                limpar() 
+                break;
+            case "←":
+                apagar() 
+                break;
+            case "%":
+                porcentagem() 
+                break;
+            case "=":
+                calcular() 
+                break;
+            default:
+                adicionar(botao.innerText)
+                break;
+        }
+    })
+}
---
+++ livrojs/cap08/js/ex8_a.js
@@ -0,0 +1,10 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("pre")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const atleta = frm.inAtleta.value
+    const idade = Number(frm.inIdade.value)
+    
+})
+
