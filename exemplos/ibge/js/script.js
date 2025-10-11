6ca5d48c5a7e206d04a2f4d4bed4b66ca00de487
thiagoO2006-th <thiagosz888888@gamil.com>
2025-10-10 21:46:55 -0300

    atualizaçoes do index e script  aula 10 10

--- exemplos/ibge/js/script.js
+++ exemplos/ibge/js/script.js
@@ -1,2 +1,29 @@
 const frm = document.querySelector("form")
 
+
+async function getData(url) {
+    const resposta = await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/" + url)
+    const data = await resposta.json();
+    return data
+}
+function carregarSelect(select, url) {
+    getData(url).then((ls) => {
+        //console.table(ls)
+        for (const i of ls) {
+            const option = document.createElement("option")
+            option.value = i.id
+            option.innerText += i.nome
+            option.innerText += i.sigla != undefined ? `(${i.sigla})` : ""
+            select.appendChild(option)
+        }
+    })
+}
+
+carregarSelect(frm.inRegiao, "regioes")
+carregarSelect(frm.inEstado, "estados")
+
+frm.inEstado.addEventListener("change", () =>{
+    const uf = frm.inEstado.value
+    frm.inMunicipio.innerHTML = ""
+    carregarSelect(frm.inMunicipio, `estados/${uf}/municipios`)
+})
