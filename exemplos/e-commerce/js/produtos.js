0f2cea924f962a6c6184e17ff71d960421f6a805
thiagoO2006-th <thiagosz888888@gamil.com>
2025-10-22 21:44:40 -0300

    e-commerce aula 22 10 - começo do produto.js

---
+++ exemplos/e-commerce/js/produtos.js
@@ -0,0 +1,56 @@
+const lsProduto = [
+    {
+        nome: "Banho de Mar",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "Passeio na Floresta",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "Trio Bom dia",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "Meus Cristais",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "Campo de lavanda",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "MInha rotina",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "Vida no Campo",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "Meu Momento",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "Flores Vivas",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    }
+]
--- exemplos/e-commerce/js/script.js
+++ exemplos/e-commerce/js/script.js
@@ -1,8 +1,12 @@
 const original = document.querySelector(".produto")
 const produtos = document.querySelector("#produtos")
 produtos.innerHTML = ""
-for (let i = 0; i < 9; i++){
+for (const p of lsProduto){
 const clone = original.cloneNode(true)
+clone.querySelector(".nome").innerText = p.nome
+clone.querySelector(".descricao").innerText = p.descricao
+clone.querySelector("img").scr = p.img
+clone.querySelector(".valor").innerText= `R$ ${p.valor.toFixed(2)}`
 produtos.appendChild(clone)
 
 }
