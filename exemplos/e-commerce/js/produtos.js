f4cdc9cd94f28d9f5c77c93e72105a7aee9bf9c2
thiagoO2006-th <thiagosz888888@gamil.com>
2025-10-23 21:50:37 -0300

    e-commerce aula 23 10: atualizaçao no index, css, produtos.js,script.js

--- exemplos/e-commerce/js/produtos.js
+++ exemplos/e-commerce/js/produtos.js
@@ -1,56 +1,56 @@
 const lsProduto = [
     {
         nome: "Banho de Mar",
-        descricao: "vela aromática - 300g",
+        descricao: "vela aromática - 150g",
         img: "img/vela-01.png",
-        valor: 39
+        valor: 39.50
     },
     {
         nome: "Passeio na Floresta",
-        descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        descricao: "vela aromática - 100g",
+        img: "img/vela-02.png",
+        valor: 19.99
     },
     {
         nome: "Trio Bom dia",
-        descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        descricao: "vela aromática - 200g",
+        img: "img/vela-03.png",
+        valor: 51.50
     },
     {
         nome: "Meus Cristais",
-        descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        descricao: "vela aromática - 100g",
+        img: "img/vela-04.png",
+        valor: 12.60
     },
     {
         nome: "Campo de lavanda",
-        descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        descricao: "vela aromática - 240g",
+        img: "img/vela-05.png",
+        valor: 45.30
     },
     {
-        nome: "MInha rotina",
+        nome: "Minha rotina",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        img: "img/vela-06.png",
+        valor: 50.70
     },
     {
         nome: "Vida no Campo",
-        descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        descricao: "vela aromática - 250g",
+        img: "img/vela-07.png",
+        valor: 35.20
     },
     {
         nome: "Meu Momento",
-        descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        descricao: "vela aromática - 190g",
+        img: "img/vela-08.png",
+        valor: 25.99
     },
     {
         nome: "Flores Vivas",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        img: "img/vela-09.png",
+        valor: 30.90
     }
 ]
--- exemplos/e-commerce/js/script.js
+++ exemplos/e-commerce/js/script.js
@@ -1,12 +1,43 @@
 const original = document.querySelector(".produto")
 const produtos = document.querySelector("#produtos")
 produtos.innerHTML = ""
-for (const p of lsProduto){
-const clone = original.cloneNode(true)
-clone.querySelector(".nome").innerText = p.nome
-clone.querySelector(".descricao").innerText = p.descricao
-clone.querySelector("img").scr = p.img
-clone.querySelector(".valor").innerText= `R$ ${p.valor.toFixed(2)}`
-produtos.appendChild(clone)
+for (const p of lsProduto) {
+    const clone = original.cloneNode(true)
+    clone.querySelector(".nome").innerText = p.nome
+    clone.querySelector(".descricao").innerText = p.descricao
+    clone.querySelector("img").src = p.img
+    clone.querySelector(".valor").innerText = `R$ ${p.valor.toFixed(2).toString().replace(".", ",")}`
+    produtos.appendChild(clone)
+
+}
+
+document.querySelectorAll(".produto").forEach((p, i) => {
+    p.addEventListener("click", () => {
+        p.classList.toggle("marcado")
+        if (lsProduto[i].marcado == undefined) {
+            lsProduto[i].marcado = 1
+        } else {
+            delete lsProduto[i].marcado
+        }
+        atualizarQt()
+
+    })
+
+})
+
+function atualizarQt() {
+    const qt = lsProduto.filter(p => p.marcado == 1)
+    document.querySelector("#qt").innerText =  qt.length > 0 ? qt.length : ""
+   
+}
+
+document.querySelector("#btVela").addEventListener("click",carrinho)
+
+function carrinho(){
+    document.querySelector("#qt").innerText
+    if(qt == ""){
+        alert("Necessário selecionar 1 item.")
+        return
+    }
 
 }
